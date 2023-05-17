848248d4159bb08825a5025dd175cb6877b03e9c
Vinay Keerthi <11478411+stonecharioteer@users.noreply.github.com>
2023-05-17 19:15:10 +0530

    fix: replace build.rs with a shell script instead since there are several problems getting the build script running

---
+++ build-docker.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# Script to build the docker image.
+# NOTE: `1.1.12` is the nym version we are using in the file explicitly.
+# So this version reflects this. Ideally, this could be replaced by an
+# official nym docker container should that exist.
+docker build -t 'chainsafe/nym:1.1.12' -f ./Dockerfile.nym .
