296309f5d361c13de949afb6ac0f54f498989aa3
Vinay Keerthi <11478411+stonecharioteer@users.noreply.github.com>
2023-05-17 20:23:04 +0530

    chore: add recommended flags to shell script

--- build-docker.sh
+++ build-docker.sh
@@ -3,4 +3,7 @@
 # NOTE: `1.1.12` is the nym version we are using in the file explicitly.
 # So this version reflects this. Ideally, this could be replaced by an
 # official nym docker container should that exist.
+set -euo pipefail
+IFS=$'\n\t'
+set -xf
 docker build -t 'chainsafe/nym:1.1.12' -f ./Dockerfile.nym .
